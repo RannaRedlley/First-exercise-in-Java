c898d8800be59ce34207996f9975e6368202b379
Ranna Redlley <146448843+RannaRedlley@users.noreply.github.com>
2023-11-22 11:42:52 -0300

    Add files via upload

---
+++ exercicio_04.java
@@ -0,0 +1,15 @@
+package fundamentos;
+
+public class exercicio_04 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a=3;
+		int b=4;
+		int c=7;
+		System.out.println(!((a>b) && (a<c)));
+
+	}
+
+}
